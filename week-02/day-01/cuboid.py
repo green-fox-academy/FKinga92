c7feae53bf85131a4520177b0042ab07f7342784
Kinga <felfalusi.kinga@gmail.com>
2017-11-27 17:33:20 +0100

    add exercise 12

---
+++ week-02/day-01/cuboid.py
@@ -0,0 +1,14 @@
+# Write a program that stores 3 sides of a cuboid as variables (doubles)
+# The program should write the surface area and volume of the cuboid like:
+# 
+# Surface Area: 600
+# Volume: 1000
+
+a = 10
+b = 10
+c = 10
+area = 2 * ( a*b + b*c + a*c)
+volume = a * b * c
+
+print("Surface Area: " + str(area))
+print("Volume: " + str(volume))
