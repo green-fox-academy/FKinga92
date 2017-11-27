856d15e6632c93d31b8b399973e477ec4a9b4326
Kinga <felfalusi.kinga@gmail.com>
2017-11-27 16:31:32 +0100

    add exercise 33

---
+++ week-02/day-01/guess_the_number.py
@@ -0,0 +1,20 @@
+# Write a program that stores a number, and the user has to figure it out.
+# The user can input guesses, after each guess the program would tell one
+# of the following:
+#
+# The stored number is higher
+# The stried number is lower
+# You found the number: 8
+
+n = 8
+x = 0
+while (n-x) != 0:
+    x = int(input("Guess the number: "))
+    if n > x:
+        print("The stored number is higher.")
+    elif n < x:
+        print ("The stored number is lower.")
+    else:
+        print("You found the number: 8")
+        break
+
