dd8a90d9eecf476bb58477d7c390037a41b235f7
Kinga <felfalusi.kinga@gmail.com>
2017-11-27 12:45:09 +0100

    add exercise 2-4

---
+++ week-02/day-01/hello_others.py
@@ -0,0 +1,10 @@
+# Greet 3 of your classmates with this program, in three separate lines
+# like:
+#
+# Hello, Esther!
+# Hello, Mary!
+# Hello, Joe!
+
+print("Hello, Emese!")
+print("Hello, Alexa!")
+print("Hello, Hédi!")
