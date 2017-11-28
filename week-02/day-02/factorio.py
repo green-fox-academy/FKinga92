78fda6cf5cefb783b321329f01d45db23473a680
Kinga <felfalusi.kinga@gmail.com>
2017-11-28 11:34:17 +0100

    add factorio exercise

---
+++ week-02/day-02/factorio.py
@@ -0,0 +1,8 @@
+# - Create a function called `factorio`
+#   that returns it's input's factorial
+
+def factorio(num):
+    fact = 1
+    for i in range(1, num+1):
+        fact *= i
+    return fact
