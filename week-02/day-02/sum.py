ca7298b89136d8b02ef9b0770d220fb8fb77f9e4
Kinga <felfalusi.kinga@gmail.com>
2017-11-28 11:30:43 +0100

    add sum exercise

---
+++ week-02/day-02/sum.py
@@ -0,0 +1,10 @@
+# - Write a function called `sum` that sum all the numbers
+#   until the given parameter
+
+def sum(num):
+    sum = 0
+    for i in range(num):
+        sum += i
+    return sum
+
+        
