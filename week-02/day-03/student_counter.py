b29f4438048c71d8ad01feead932c1364187f9cf
Kinga <felfalusi.kinga@gmail.com>
2017-11-29 10:38:40 +0100

    add student counter exercise

---
+++ week-02/day-03/student_counter.py
@@ -0,0 +1,32 @@
+students = [
+        {'name': 'Teodor', 'age': 3, 'candies': 2},
+        {'name': 'Rezso', 'age': 9.5, 'candies': 2},
+        {'name': 'Zsombor', 'age': 12, 'candies': 5},
+        {'name': 'Aurel', 'age': 7, 'candies': 3},
+        {'name': 'Olaf', 'age': 12, 'candies': 7},
+        {'name': 'Gerzson', 'age': 10, 'candies': 1}
+]
+
+# create a function that takes a list of students and prints: 
+# - how many candies are owned by students
+
+# create a function that takes a list of students and prints:
+# - Sum of the age of people who have lass than 5 candies
+
+def candies(*args):
+    list1 = [*args]
+    for i in list1:
+        student = str(i)
+        student_index = next(index for (index, d) in enumerate(students) if d["name"] == student)
+        print(student + " has " + str(students[student_index]['candies']) + " candies.")
+
+
+def sum_of_ages(*args):
+    list1 = [*args]
+    sum = 0
+    for i in list1:
+        student = str(i)
+        student_index = next(index for (index, d) in enumerate(students) if d["name"] == student)
+        if int(students[student_index]['candies']) < 5:
+            sum += float(students[student_index]['age'])
+    print("The sum of the age of people who have less than 5 candies: " + str(sum))
