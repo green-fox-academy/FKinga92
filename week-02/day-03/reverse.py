8b41bc22056dba24daf2d6e93f48bd1ba1fb4cef
Kinga <felfalusi.kinga@gmail.com>
2017-11-29 11:07:22 +0100

    add reverse exercise

---
+++ week-02/day-03/reverse.py
@@ -0,0 +1,12 @@
+# Create a function called 'reverse_string' which takes a string as a parameter
+# The function reverses it and returns with the reversed string
+
+
+reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI"
+
+def reverse_string(st = "abc"):
+    st = st[: : -1]
+    return print(st)
+
+
+reverse_string(reversed)
