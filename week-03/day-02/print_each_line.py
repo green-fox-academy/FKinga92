618d1778f8178a2387546a3c21dd2f7a6f0ad544
Kinga <felfalusi.kinga@gmail.com>
2017-12-05 11:48:17 +0100

    add solution for exercise 02

---
+++ week-03/day-02/print_each_line.py
@@ -0,0 +1,13 @@
+# Write a program that opens a file called "my-file.txt", then prints
+# each of lines form the file.
+# If the program is unable to read the file (for example it does not exists),
+# then it should print an error message like: "Unable to read file: my-file.txt"
+
+file_name = "my-file.txt"
+try:
+    f = open(file_name, "r")
+    for line in f:
+        print(line.rstrip())
+    f.close()
+except IOError:
+    print("Unable to read file: ", file_name)
