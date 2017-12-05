f1e19a716ba18bf997ef9df9826294775569b51f
Kinga <felfalusi.kinga@gmail.com>
2017-12-05 12:16:33 +0100

    add solution to exercise 04

---
+++ week-03/day-02/write_single_line.py
@@ -0,0 +1,10 @@
+# Open a file called "my-file.txt"
+# Write your name in it as a single line
+# If the program is unable to write the file,
+# then it should print an error message like: "Unable to write file: my-file.txt"
+
+f = open("my-file.txt", "w")
+try:
+    f.write("Kinga")
+except:
+    print("Unable to write file: my-file.txt")
