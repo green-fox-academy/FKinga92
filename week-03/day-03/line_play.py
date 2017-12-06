ca4f1f30ab9597a421d53ecc42edc2ec9717e7ab
Kinga <felfalusi.kinga@gmail.com>
2017-12-06 15:44:44 +0100

    fix exercise 15

--- week-03/day-03/line_play.py
+++ week-03/day-03/line_play.py
@@ -11,11 +11,11 @@ canvas.pack()
 def draw_purple_line(x, range_start, range_end):
     line1 = canvas.create_line(x, range_start, range_end, x, fill="purple")
 
-def draw_green_line(x):
-    line2 = canvas.create_line(0, x, x, 300, fill="green")
+def draw_green_line(x, range_start, range_end):
+    line2 = canvas.create_line(range_start, x, x, range_end, fill="green")
 
 for i in range(0, 300, 20):
     draw_purple_line(i, 0, 300)
-    draw_green_line(i)
+    draw_green_line(i, 0, 300)
 
 root.mainloop()
