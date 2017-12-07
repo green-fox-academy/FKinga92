96cb457b7f8476cdd6e394abd548c164984800d4
Kinga <felfalusi.kinga@gmail.com>
2017-12-07 10:03:50 +0100

    add exercise 01

---
+++ week-03/day-04/01counter.py
@@ -0,0 +1,10 @@
+# Write a recursive function that takes one parameter: n and counts down from n.
+
+def counter(n):
+    if n == 0:
+        return n
+    else:
+        return str(n) + "\n" + str(counter(n - 1))
+
+
+print(counter(10))
