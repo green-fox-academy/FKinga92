f7b7ec92a9f0740dfb7159e1f4e6a17e24712f9e
Kinga <felfalusi.kinga@gmail.com>
2017-12-07 22:40:39 +0100

    fix solution for exercise 16

--- week-03/day-03/line_play_quarters.py
+++ week-03/day-03/line_play_quarters.py
@@ -2,47 +2,24 @@ from tkinter import *
 
 root = Tk()
 
-canvas = Canvas(root, width='300', height='300')
+size = 300
+s2 = size // 2
+canvas = Canvas(root, width=size, height=size)
 canvas.pack()
 
 # divide the canvas into 4 equal parts
 # and repeat this pattern in each quarter:
 # [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/line-play/r1.png]
 
-def draw_purple_line1(x, range_start, range_end):
-    line1 = canvas.create_line(x, range_start, range_end, x, fill="purple")
+def quarter(x, y, x1, y1):
+    for i in range(0, s2, 10):
+        purple_line = canvas.create_line(x + i, y, x1, y + i, fill="purple")
+        green_line = canvas.create_line(x, y + i, x + i, y1, fill="green")
 
-def draw_green_line1(x, range_start, range_end):
-    line2 = canvas.create_line(range_start, x, x, range_end, fill="green")
-
-def draw_purple_line2(x, range_start, range_end):
-    line1 = canvas.create_line(x, 0, range_end, x-range_start, fill="purple")
-
-def draw_green_line2(x, range_end):
-    line2 = canvas.create_line(range_end, x, range_end+x, range_end, fill="green")
-
-def draw_purple_line3(x, range_end):
-    line1 = canvas.create_line(x, range_end, range_end, range_end+x, fill="purple")
-
-def draw_green_line3(x, range_start, range_end):
-    line2 = canvas.create_line(0, x, x-range_start, range_end, fill="green")
-
-for i in range(0, 150, 20):
-    draw_purple_line1(i, 0, 150)
-    draw_green_line1(i, 0, 150)
-    draw_green_line2(i, 150)
-    draw_purple_line3(i, 150)
-    
-for i in range(150, 300, 20):
-    draw_purple_line1(i, 150, 300)
-    draw_green_line1(i, 150, 300)
-    draw_purple_line2(i, 150, 300)
-    draw_green_line3(i, 150, 300)
-
-# hétvégén átalakítom :)
-   
-
-
-
+quarter(0, 0, s2, s2)
+quarter(0, s2, s2, size)
+quarter(s2, 0, size, s2)
+quarter(s2, s2, size, size)
 
 root.mainloop()
+
