dcdb0cb7c31f5127c188a254b26b28933406e43e
Kinga <felfalusi.kinga@gmail.com>
2017-12-18 16:37:52 +0100

    add game map

---
+++ week-05/view.py
@@ -0,0 +1,41 @@
+from tkinter import *
+from model import *
+
+root = Tk()
+canvas = Canvas(root, width=720, height=720)
+
+class View(object):
+
+    def __init__(self):
+       self.floor = PhotoImage(file="floor.gif")
+       self.wall = PhotoImage(file="wall.gif")
+
+    def draw_tile(self, tile):
+        if not isinstance(tile, Tile):
+            return
+        self.img = PhotoImage(file=tile.image)
+        canvas.create_image(tile.x, tile.y, image=self.img)
+    
+    def draw_map(self, map):
+        if not isinstance(map, Map):
+            return
+        for i in range(len(map.map)):
+            for j in range(len(map.map[i])):
+                x = map.x + j * 72
+                y = map.y + i * 72
+                if map.map[i][j] == 0:
+                    canvas.create_image(x, y, image=self.floor)
+                else:
+                    canvas.create_image(x, y, image=self.wall)
+
+            
+
+
+
+
+view = View()
+map = Map(36, 36)
+canvas.pack()
+view.draw_map(map)
+
+root.mainloop()
