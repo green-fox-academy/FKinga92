59864683d111f04cf9fd84934a766cbfc4435fab
Kinga <felfalusi.kinga@gmail.com>
2017-12-21 12:09:34 +0100

    add solution for exercise 01

---
+++ week-05/pallida basics exam/unique_chars.py
@@ -0,0 +1,15 @@
+# Create a function called `unique_characters` that takes a string as parameter
+# and returns a list with the unique letters of the given string
+# Create basic unit tests for it with at least 3 different test cases
+# print(unique_characters("anagram"))
+# # Should print out:
+# # ["n", "g", "r", "m"]
+
+def unique_characters(string):
+    list_of_unique_characters = []
+    for char in string.replace(" ", ""):
+        if string.count(char) == 1:
+            list_of_unique_characters.append(char)
+    return list_of_unique_characters
+
+# print(unique_characters("anagram"))
