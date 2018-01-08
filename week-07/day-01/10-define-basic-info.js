93bef9ffa3d10c0c9002ac15e533e5afc264e9c7
Kinga <felfalusi.kinga@gmail.com>
2018-01-08 12:23:24 +0100

    add exercise 10

---
+++ week-07/day-01/10-define-basic-info.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+
+let name = 'Kinga';
+let age = 25;
+let height = 1.68;
+let is_married = false;
+
+console.log(name);
+console.log(age);
+console.log(height);
+console.log(is_married);
