4fbced6c9e2488fa004187e8ff9c8208381452e8
Kinga <felfalusi.kinga@gmail.com>
2018-01-09 19:54:06 +0100

    fix solution for exercise 31

--- week-07/day-02/31-is-in-list.js
+++ week-07/day-02/31-is-in-list.js
@@ -6,10 +6,10 @@
 let listOfNumbers = [2, 4, 6, 8, 10, 12, 14, 16];
 
 function numChecker(arrayToCheck) {
-  return (arrayToCheck.indexOf(4) > 0
-          && arrayToCheck.indexOf(8) > 0
-          && arrayToCheck.indexOf(12) > 0
-          && arrayToCheck.indexOf(16) > 0
+  return (arrayToCheck.indexOf(4) >= 0
+          && arrayToCheck.indexOf(8) >= 0
+          && arrayToCheck.indexOf(12) >= 0
+          && arrayToCheck.indexOf(16) >= 0
   );
 }
 
