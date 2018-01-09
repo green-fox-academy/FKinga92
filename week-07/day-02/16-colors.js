4648a466ce01b7cf7ec00c90185b3835895e3449
Kinga <felfalusi.kinga@gmail.com>
2018-01-09 14:17:52 +0100

    add solution for exercise 16

---
+++ week-07/day-02/16-colors.js
@@ -0,0 +1,20 @@
+'use strict';
+// - Create a two dimensional list
+//   which can contain the different shades of specified colors
+// - In `colors[0]` store the shades of green:
+//   `'lime', 'forest green', 'olive', 'pale green', 'spring green'`
+// - In `colors[1]` store the shades of red:
+//   `'orange red', 'red', 'tomato'`
+// - In `colors[2]` store the shades of pink:
+//   `'orchid', 'violet', 'pink', 'hot pink'`
+
+let colors = [];
+let shadesOfGreen = ['lime', 'forest green', 'olive', 'pale green', 'spring green'];
+let shadesOfRed = ['orange red', 'red', 'tomato'];
+let shadesOfPink = ['orchid', 'violet', 'pink', 'hot pink']
+
+colors.push(shadesOfGreen, shadesOfRed, shadesOfPink);
+
+console.log(colors[0])
+console.log(colors[1])
+console.log(colors[2])
