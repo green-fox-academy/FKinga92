626efd3e2a30c708f1cad36235ba597b69b94cda
Kinga <felfalusi.kinga@gmail.com>
2018-01-09 15:09:25 +0100

    add solution for exercise 19

---
+++ week-07/day-02/19-reverse.js
@@ -0,0 +1,18 @@
+'use strict';
+// - Create a variable named `aj`
+//   with the following content: `[3, 4, 5, 6, 7]`
+// - Reverse the order of the elements in `aj`
+// 		- do it with the built in method
+//		- do it with creating a new temp array and a loop
+// - Print the elements of the reversed `aj`
+
+let aj = [3, 4, 5, 6, 7];
+aj.reverse();
+console.log(aj);
+aj.reverse();
+let temp = []
+for (let i = aj.length -1; i >= 0; i--) {
+  temp.push(aj[i]);
+}
+aj = temp;
+console.log(aj.join('\n'));
