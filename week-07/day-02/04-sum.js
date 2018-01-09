15dd0362b11a5eb249169d3abd8269d2cddfa5af
Kinga <felfalusi.kinga@gmail.com>
2018-01-09 12:15:12 +0100

    add solution for exercise 04

---
+++ week-07/day-02/04-sum.js
@@ -0,0 +1,11 @@
+'use strict';
+// - Write a function called `sum` that sum all the numbers until the given parameter
+// - The function should return the result
+
+function sum(rangeEnd) {
+  let sum = 0;
+  for (let i = 1; i < rangeEnd; i++) {
+    sum += i;
+  }
+  return sum;
+}
