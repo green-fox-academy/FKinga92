9258c52d104eb03a1c4ab59ca8c9e5929fe3f473
Kinga <felfalusi.kinga@gmail.com>
2018-01-09 14:47:22 +0100

    add solution for exercise 19

---
+++ week-07/day-02/18-diagonal-matrix.js
@@ -0,0 +1,28 @@
+'use strict';
+// - Create (dynamically*) a two dimensional list
+//   with the following matrix**. Use a loop!
+//
+//   0 0 0 1
+//   0 0 1 0
+//   0 1 0 0
+//   1 0 0 0
+//
+// - Print this two dimensional list to the console
+//
+// * size should depend on a variable
+// ** Relax, a matrix is just like an array
+
+let matrixSize = 4;
+let matrix = [];
+
+for (let i = matrixSize - 1; i >= 0; i--) {
+  let matrixRow = [];
+  for (let j = 0; j  < matrixSize; j++) {
+    (j === i) ? matrixRow.push(1) : matrixRow.push(0);
+  }
+  matrix.push(matrixRow);
+}
+
+matrix.forEach(function(item) {
+  console.log(item.join(' '));
+})
