420c850cbed494e01977b37d6fb4374d9b10ebf6
Kinga <felfalusi.kinga@gmail.com>
2018-01-09 16:47:46 +0100

    add solution for exercise 30

---
+++ week-07/day-02/30-element-finder.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// Check if the array contains "7" if it contains print "Hoorray" otherwise print "Noooooo"
+
+let numbers = [1, 2, 3, 4, 5, 6, 8];
+
+(numbers.indexOf(7) > 0) ? console.log('Hoorray') : console.log('Noooooo');
