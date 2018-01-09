85cf5894058107d8a9394692277d24ad06b126be
Kinga <felfalusi.kinga@gmail.com>
2018-01-09 14:09:05 +0100

    add solution for exercise 14

---
+++ week-07/day-02/14-append-a.js
@@ -0,0 +1,10 @@
+'use strict';
+// - Create a variable named `nimals`
+//   with the following content: `['kuty', 'macsk', 'cic']`
+// - Add all elements an `'a'` at the end
+// - try to use built in functions instead of loops
+
+let nimals = ['kuty', 'macsk', 'cic'];
+nimals.map(function(item, index) {
+  nimals[index] = item + 'a';
+})
