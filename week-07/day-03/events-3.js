10d80bdb77652890319d8f027cc051abd55bbe09
Kinga <felfalusi.kinga@gmail.com>
2018-01-10 15:56:13 +0100

    add solution for exercise 13

---
+++ week-07/day-03/events-3.js
@@ -0,0 +1,9 @@
+// 1) Subsrcibe to keyup events on the global window object
+// 2) Console log the event object and peek inside
+// 3) Display the last pressed key's code as "Last pressed key code is: __"
+
+
+window.addEventListener('keyup', function (event) {
+  console.log(event);
+  document.querySelector('h1').textContent = 'Last pressed key code is: ' + event.key;
+});
