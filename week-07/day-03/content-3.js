e334a990af9970f21926a2398e82129c55d12b19
Kinga <felfalusi.kinga@gmail.com>
2018-01-10 12:38:53 +0100

    add solution for exercise 04

---
+++ week-07/day-03/content-3.js
@@ -0,0 +1,6 @@
+// You can work in the html or in a separate js file. Like:
+//   fill output1 with the first paragraph's content, text only.
+//   fill output2 with the first paragraph's content keeping the cat strong.
+
+output1 = document.querySelector('p').textContent;
+output2 = document.querySelector('p').innerHTML;
