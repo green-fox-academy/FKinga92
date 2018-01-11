112d5c1ee16200a325309e2f18ea7206b5b7f370
Kinga <felfalusi.kinga@gmail.com>
2018-01-11 11:58:20 +0100

    feat(imageSlider): add slideLift function and left arrow click event

--- week-07/image-slider/image-slider.js
+++ week-07/image-slider/image-slider.js
@@ -1,4 +1,6 @@
 let images = document.querySelectorAll('.images');
+let arrowLeft = document.querySelectorAll('img')[0];
+let arrowRight = document.querySelector('.arrow-right').getElementsByTagName('img')[0];
 let current = 0;
 
 function reset() {
@@ -11,5 +13,18 @@ function startSlide() {
   reset();
   images[0].style.display = 'block';
 }
+
+function slideLeft() {
+  reset();
+  images[current - 1].style.display = 'block';
+  current--;
+}
+
+arrowLeft.addEventListener('click', function() {
+  if (current === 0) {
+    current = images.length;
+  }
+  slideLeft();
+})
 
 startSlide();
