559b4d6732929df28ec413625da9136bf52b90fe
Kinga <felfalusi.kinga@gmail.com>
2018-01-17 17:16:21 +0100

    add solution for exercise 03

---
+++ week-08/day-03/task3-get-position/index.js
@@ -0,0 +1,50 @@
+'use stric';
+
+let body = document.querySelector('body');
+let button = document.querySelector('button');
+let clickCounter = 0;
+button.addEventListener('click', getResponse);
+
+function getResponse() {
+  let city = document.getElementById('location').value.split(' ').join('+');
+  requestCityLocation(city);
+  createMap(city);
+  clickCounter++;
+}
+
+function displayCoordinates(city) {
+  if (clickCounter <= 1) {
+    let locationPar = document.createElement('p');
+    locationPar.textContent = 'Your city\'s coordinates: ' + city.lat + ', ' + city.lon;
+    body.appendChild(locationPar);
+  } else {
+    document.querySelector('p').textContent = 'Your city\'s coordinates: ' + city.lat + ', ' + city.lon;
+  }
+}
+
+function requestCityLocation(city) {
+  let xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      let cityPosition = JSON.parse(xhr.response).Results[0];
+      displayCoordinates(cityPosition);
+    }
+  };
+  xhr.open('GET', `https://devru-latitude-longitude-find-v1.p.mashape.com/latlon.php?location=${city}`);
+  xhr.setRequestHeader('X-Mashape-Key', 'wYyasdh3BZmsh1REIo5FUJZzqTpYp1VrYmHjsnqeaX5dkES2Zx');
+  xhr.send();
+}
+
+function createMap(city) {
+  if (clickCounter < 1) {
+    let map = document.createElement('iframe');
+    map.width = '600';
+    map.height = '450';
+    map.frameborder = '0';
+    map.style = 'border: 0';
+    map.src = `https://www.google.com/maps/embed/v1/place?key=AIzaSyBFf7IRxT68e-9DlS0SlkPv2hb8pp_7M4U&q=${city}`;
+    body.appendChild(map);
+  } else {
+    document.querySelector('iframe').src = `https://www.google.com/maps/embed/v1/place?key=AIzaSyBFf7IRxT68e-9DlS0SlkPv2hb8pp_7M4U&q=${city}`;
+  }
+}
