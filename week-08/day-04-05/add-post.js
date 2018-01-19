d719eb23cf7425b37bc7c5abfec364c228349038
Kinga <felfalusi.kinga@gmail.com>
2018-01-19 14:49:25 +0100

    fix submit post function

--- week-08/day-04-05/add-post.js
+++ week-08/day-04-05/add-post.js
@@ -1,21 +1,21 @@
 'use strict';
 
-function inputToPostObject() {
-  let postTitle = document.querySelector('textarea').value;
-  let postUrl = document.getElementById('url').value;
-  if (postTitle === '' || postUrl === '') {
-    alert('You cannot send a post with empty fields. Please enter a title and URL.');
-    return;
-  }
-  return {
-    "title": postTitle,
-    "url": postUrl,
-    "timestamp": Date.now(),
-    "owner": 'anonymous'
-  }
+function isValidInput(postTitle, postUrl) {
+  return (postTitle !== '' && postUrl !== '');
 }
 
-function getResponse() {
+function inputToPostObject(postTitle, postUrl) {
+    return {
+      "title": postTitle,
+      "url": postUrl,
+      "timestamp": Date.now(),
+      "owner": "anonymous"
+    };
+}
+
+function submitPost() {
+  let postTitle = document.querySelector('textarea').value;
+  let postUrl = document.getElementById('url').value;
   let xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
     if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
@@ -25,7 +25,11 @@ function getResponse() {
   xhr.open('POST', 'https://time-radish.glitch.me/posts');
   xhr.setRequestHeader('accept', 'application/json');
   xhr.setRequestHeader('content-type', 'application/json');
-  xhr.send(JSON.stringify(inputToPostObject()));
+  if (isValidInput(postTitle, postUrl)) {
+    xhr.send(JSON.stringify(inputToPostObject(postTitle, postUrl)));
+  } else {
+    alert('You cannot send a post with empty fields. Please enter a title and URL.');
+  }
 }
 
-document.querySelector('button').addEventListener('click', getResponse);
+document.querySelector('button').addEventListener('click', submitPost);
