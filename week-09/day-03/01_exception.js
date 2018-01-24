aa43e6e8e092aeb16220bd137d7fc8fb648715bc
Kinga <felfalusi.kinga@gmail.com>
2018-01-24 22:23:32 +0100

    fix styling

--- week-09/day-03/01_exception.js
+++ week-09/day-03/01_exception.js
@@ -1,26 +1,24 @@
-'use strict';
-
 // Handle the exceptions in the addString() function. It should check the type of the
 // arguments, throw the right error and write it to the console.
 // It should add the strings too if the arguments are appropriate.
 
-let addString = function(str1, str2, printStr) {
+const addString = function (str1, str2, printStr) {
   try {
-    let newStr = str1 + str2;
+    const newStr = str1 + str2;
     if (typeof str1 !== 'string') {
-      throw new Error(str1 + ' is not a string');
+      throw new Error(`${str1} is not a string`);
     } else if (typeof str2 !== 'string') {
-      throw new Error(str2 + ' is not a string');
+      throw new Error(`${str2} is not a string`);
     }
     printStr(newStr);
   } catch (err) {
-    console.log('Error: ' + err.message);
-  }  
-}
+    console.log(`Error: ${err.message}`);
+  }
+};
 
-let printStr = function(str) {
+const printStr = function (str) {
   console.log(str);
-}
+};
 
 addString(1234, 56789, 'printStr');
 addString('alma', 56789, 'printStr');
