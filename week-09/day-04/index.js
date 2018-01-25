03911c65c282a1ec94f5c4075eb6623b75551988
Kinga <felfalusi.kinga@gmail.com>
2018-01-25 12:15:13 +0100

    add solution for doubling exercise

--- week-09/day-04/index.js
+++ week-09/day-04/index.js
@@ -9,6 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/doubling', function (req, res) {
+  const input = req.query.input;
+  if (input === undefined) {
+    res.json({error: 'Please provide an input!'});
+  } else {
+    res.json({received: input, result: input * 2});
+  }
+});
+
 app.listen(8080, function () {
   console.log('the app is running');
 });
