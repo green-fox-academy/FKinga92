ad9e553c8e77fba8ef62680d3653d3c0bae3181a
Kinga <felfalusi.kinga@gmail.com>
2018-01-25 13:11:07 +0100

    add solution for do until exercise

--- week-09/day-04/index.js
+++ week-09/day-04/index.js
@@ -2,8 +2,11 @@
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 app.use('/assets', express.static('./assets'));
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
@@ -12,34 +15,92 @@ app.get('/', function (req, res) {
 app.get('/doubling', function (req, res) {
   const input = req.query.input;
   if (input === undefined) {
-    res.json({error: 'Please provide an input!'});
+    res.json(
+      {
+        error: 'Please provide an input!'
+      }
+    );
   } else {
-    res.json({received: Number(input), result: input * 2});
+    res.json(
+      {
+        received: Number(input),
+        result: input * 2
+      }
+    );
   }
 });
 
 app.get('/greeter', function (req, res) {
   const input = req.query;
   if (input.hasOwnProperty('name') === false) {
-    res.json({
-      error: 'Please provide a name!'
-    });
+    res.json(
+      {
+        error: 'Please provide a name!'
+      }
+    );
   } else if (input.hasOwnProperty('title') === false) {
-    res.json({
-      error: 'Please provide a title!'
-    });
+    res.json(
+      {
+        error: 'Please provide a title!'
+      }
+    );
   } else {
-    res.json({
-      welcome_message: `Oh, hi there ${input.name}, my dear ${input.title}!`
-    });
+    res.json(
+      {
+        welcome_message: `Oh, hi there ${input.name}, my dear ${input.title}!`
+      }
+    );
   }
 });
 
 app.get('/appenda/:appenda', function (req, res) {
   const appendable = req.params.appenda;
-  res.json({
-    appended: `${appendable}a`
-  });
+  res.json(
+    {
+      appended: `${appendable}a`
+    }
+  );
+});
+
+function getSumUntil(number) {
+  let sum = 0;
+  for (let i = 0; i <= number; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+function getFactorial(number) {
+  let factorial = 1;
+  for (let i = 1; i <= number; i++) {
+    factorial *= i;
+  }
+  return factorial;
+}
+
+app.post('/dountil/:operation', function (req, res) {
+  const operation = req.params.operation;
+  if (req.body.hasOwnProperty('until') === false) {
+    res.json(
+      {
+        error: 'Please provide a number!'
+      }
+    );
+  }
+
+  if (operation === 'sum') {
+    res.json(
+      {
+        result: getSumUntil(req.body.until)
+      }
+    );
+  } else if (operation === 'factor') {
+    res.json(
+      {
+        result: getFactorial(req.body.until)
+      }
+  );
+  }  
 });
 
 app.listen(8080, function () {
