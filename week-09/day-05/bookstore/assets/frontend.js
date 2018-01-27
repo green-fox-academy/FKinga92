de376738dca5a42dfb2c99bfa786c02145698015
Kinga <felfalusi.kinga@gmail.com>
2018-01-27 20:17:02 +0100

    add filtering option to bookstore exercise

--- week-09/day-05/bookstore/assets/frontend.js
+++ week-09/day-05/bookstore/assets/frontend.js
@@ -3,8 +3,9 @@
 const buttons = document.querySelectorAll('button');
 const section = document.querySelector('section');
 const main = document.querySelector('main');
+const divs = document.querySelectorAll('div');
 
-function ajax(method, url, callback) {
+function sendRequest(method, url, callback) {
   const xhr = new XMLHttpRequest;
   xhr.onreadystatechange = () => {
     if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
@@ -47,11 +48,56 @@ function showFullBookData(books) {
   main.innerHTML = markup;
 }
 
+function getQuery() {
+  let urlWithQuerys = 'http://localhost:8080/books?';
+  let categoryFilter = document.getElementById('category').value;
+  if (categoryFilter !== '') {
+    urlWithQuerys += `&category=${categoryFilter}`;
+  }
+  let publisherFilter = document.getElementById('pub_name').value;
+  if (publisherFilter !== '') {
+    urlWithQuerys += `&publisher=${publisherFilter}`;
+  }
+  let priceLowerFilter = document.getElementById('plt').value;
+  if (priceLowerFilter !== '') {
+    urlWithQuerys += `&plt=${priceLowerFilter}`;
+  }
+  let priceGreaterFilter = document.getElementById('pgt').value;
+  if (priceGreaterFilter !== '') {
+    urlWithQuerys += `&pgt=${priceGreaterFilter}`;
+  }
+  urlWithQuerys = urlWithQuerys.replace('?&', '?');
+  return urlWithQuerys;
+}
+
+function showFilteredBookData(books) {
+  let markup = '<table><thead><tr><th>Book Title</th> ' +
+               '<th>Author name</th><th>Category</th> ' +
+               '<th>Publisher\'s name</th><th>Price</th>' +
+               '</tr></thead><tbody>';
+  books.forEach((book) => {
+    markup += `<tr>
+                <td>${book.book_name}</td>
+                <td>${book.aut_name}</td>
+                <td>${book.cate_descrip}</td>
+                <td>${book.pub_name}</td>
+                <td>${book.book_price}</td>
+               </tr>`;
+  });
+  markup += '</tbody></table>';
+  divs[2].innerHTML = markup;
+}
+
 buttons[0].addEventListener('click', () => {
-  ajax('GET', 'http://localhost:8080/titles', listBookTitles);
+  sendRequest('GET', 'http://localhost:8080/titles', listBookTitles);
 });
 buttons[1].addEventListener('click', () => {
-  ajax('GET', 'http://localhost:8080/books', showFullBookData);
+  sendRequest('GET', 'http://localhost:8080/books', showFullBookData);
+});
+document.querySelector('form').addEventListener('submit', (event) => {
+  event.preventDefault();
+  let url = getQuery();
+  sendRequest('GET', url, showFilteredBookData);
 });
 
 
--- week-09/day-05/bookstore/index.js
+++ week-09/day-05/bookstore/index.js
@@ -30,12 +30,32 @@ app.get('/titles', (req, res) => {
 });
 
 app.get('/books', (req, res) => {
-  let queryString = 'SELECT book_name, aut_name, cate_descrip, pub_name, book_price ' +
-                    'FROM book_mast ' +
-                    'INNER JOIN author ON book_mast.aut_id = author.aut_id ' +
-                    'INNER JOIN category ON book_mast.cate_id = category.cate_id ' +
-                    'INNER JOIN newpublisher ON book_mast.pub_id = newpublisher.pub_id; '
-  conn.query(queryString, (err, rows) => {
+  const requestQuery = req.query;
+  let myQuery = 'SELECT book_name, aut_name, cate_descrip, pub_name, book_price ' +
+                'FROM book_mast ' +
+                'INNER JOIN author ON book_mast.aut_id = author.aut_id ' +
+                'INNER JOIN category ON book_mast.cate_id = category.cate_id ' +
+                'INNER JOIN newpublisher ON book_mast.pub_id = newpublisher.pub_id ';
+  
+  if (Object.keys(requestQuery).length > 0) {
+    myQuery += 'WHERE ';
+    if (requestQuery.hasOwnProperty('category')) {
+      myQuery += `AND cate_descrip LIKE '%${requestQuery.category}%'`;
+    }
+    if (requestQuery.hasOwnProperty('publisher')) {
+      myQuery += `AND pub_name LIKE '%${requestQuery.publisher}%'`;
+    }
+    if (requestQuery.hasOwnProperty('plt')) {
+      myQuery += `AND book_price < ${requestQuery.plt}`;
+    }
+    if (requestQuery.hasOwnProperty('pgt')) {
+      myQuery += `AND book_price > ${requestQuery.pgt}`;
+    }
+    myQuery = myQuery.replace('WHERE AND ', 'WHERE ');
+    myQuery += ';'
+  }
+
+  conn.query(myQuery, (err, rows) => {
     if(err) {
       console.log(err.toString());
       res.status(500).send('Database error');
@@ -44,7 +64,6 @@ app.get('/books', (req, res) => {
     res.send(rows);
   })
 });
-
 
 app.listen(8080, () => {
   console.log('the app is listening.');
